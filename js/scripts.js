359dcf3940b0a6d634fe8a8109714ba2ddbb165b
Cristina Plesa <cristinaplesa8@gmail.com>
2021-03-26 15:42:02 -0700

    BUGFIX: fixed selector layout. Moved on to writing submit function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-  $("form#user-input").submit(function() {
+  
+  // $("")
+  // const $("input:radio[name=mathQuestionInitial]:checked").val();
 
+  //   if ()
+
+  // });
+
+  $("form#user-input").submit(function(event) {
+    const userInput1 = $("#question-input1").val();
+    const userInput2 = $("#question-input2").val();
+    const userInput3 = $("#question-input3").val();
+    const userInput5 = $("#question-input5").val();
+
+    // if ()
+    event.preventDefault();
   });
 });
